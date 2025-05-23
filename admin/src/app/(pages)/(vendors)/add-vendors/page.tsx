b39c242b5cc1089abe121d6683c021046eb095a4
Mahamud Pino <mahamudpino24678@gmail.com>
2025-05-23 22:56:07 +0600

    add

--- admin/src/app/(pages)/(vendors)/add-vendors/page.tsx
+++ admin/src/app/(pages)/(vendors)/add-vendors/page.tsx
@@ -6,17 +6,25 @@ import VendorForm from "@/components/pageComponents/VendorForm";
 
 const AddVendors: React.FC = () => {
   const additionalFields = <VendorForm vendor={null} />;
-
+  const photosData = {
+    image: [],
+  };
   return (
     <AddForm
       endpoint={`${process.env.NEXT_PUBLIC_BASEURL}/v1/admin/vendors`}
       additionalFields={additionalFields}
       buttonText="Add Vendor"
       id=""
-      //@ts-ignore
-      photosData={[]}
-      isNoPhotoFile={true}
+      photosData={photosData}
       link="vendors-list"
+      imageFields={[
+        {
+          key: "image",
+          isMultiple: false,
+          label: "Size must be (1920*860)",
+          isArray: false,
+        },
+      ]}
     />
   );
 };
